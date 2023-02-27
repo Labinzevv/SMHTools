39f2c57dd0b59d0b044924c20e4d1845d2e9c9d7
Labinzevv <labinzevv@gmail.com>
2023-02-27 23:59:36 +0300

    change colorPercent

--- tools/colorPercent/colorPercent.cs
+++ tools/colorPercent/colorPercent.cs
@@ -6,20 +6,23 @@ using UnityEngine.UI;
 public class colorPercent : MonoBehaviour
 {
     //calculates the percentage of color shades in the image
-    //important!!! it is necessary to change texture format to rgba 32 bit in inspector (also FilterMode = Point and Texture Wrap Mode = Clamp)
+    //important!!! it is necessary to change texture format to rgba 32 bit in inspector
+    //(also FilterMode = Point and Texture Wrap Mode = Clamp, flag read/write enable = true)
+    //only quad images
 
     public Text redText;
     public Text greenText;
     public Text blueText;
+    public Text shadesGreyText;
     public Texture2D texture;
     public FilterMode filterMode = FilterMode.Point;
     public TextureWrapMode textureWrapMode = TextureWrapMode.Clamp;
-
 
     int maxPixel = 0;
     float r = 0;
     float g = 0;
     float b = 0;
+    float shadesGrey = 0;
     Color color;
 
     private void Start()
@@ -59,12 +62,18 @@ public class colorPercent : MonoBehaviour
                             {
                                 b++;
                             }
+                            if (color.b == color.r && color.b == color.g)
+                            {
+                                shadesGrey++;
+                            }
                             float R = (r / (texture.height * texture.width)) * 100;
                             float G = (g / (texture.height * texture.width)) * 100;
                             float B = (b / (texture.height * texture.width)) * 100;
+                            float ShadesGrey = (shadesGrey / (texture.height * texture.width)) * 100;
                             redText.text = "pixels of red hue count: " + R.ToString("0.0") + " percent";
                             greenText.text = "pixels of green hue count: " + G.ToString("0.0") + " percent";
                             blueText.text = "pixels of blue hue count: " + B.ToString("0.0") + " percent";
+                            shadesGreyText.text = "grayscale pixels count: " + ShadesGrey.ToString("0.0") + " percent";
                             maxPixel++;
                         }
                     }
